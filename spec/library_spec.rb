a70e3bbb241b7e314ff71389990cb3d355a701f8
Katie Hoffman <ktahoffman@gmail.com>
2015-03-29 15:16:47 -0400

    make it text for last test more descriptive

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -33,10 +33,10 @@ describe "filled out migrations:" do
   end
 end
 
-describe "answered questions:" do
-  it "which one of the original eight migrations made a join table?" do
-    # for example, if the 01_create_books.rb migration made a join table, you would write:
-    # file_name = "01_create_books.rb"
+describe "answered questions correctly:" do
+  it "which one of the original eight migrations was a join table between memebers and books?" do
+    # for example, if the 07_create_genres.rb migration made the join table, you would write:
+    # file_name = "07_create_genres.rb"
     file_name = __
     expect(encode(file_name)).to eq("578733af48e2ed4890e138fc3183f211ea1878fe")
   end
