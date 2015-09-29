116d724b9c53643e66246dc790abe367cc7fa763
msuzoagu <msuzoagu@gmail.com>
2015-09-29 13:23:17 -0400

    create books table with correct attributes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,15 +1,11 @@
 class Book < ActiveRecord::Base
-<<<<<<< HEAD
   belongs_to :author
   belongs_to :genre
   belongs_to :topic
-  
+
   belongs_to :library
   delegate :county, to: :library
 
   has_many :checkouts
   has_many :readers, :through => :checkouts
 end
-=======
-end
->>>>>>> model test for name and description attribute for books table
