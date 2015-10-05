16144e39dd6e31ec1911006323472c13092b7e37
msuzoagu <msuzoagu@gmail.com>
2015-10-05 15:37:15 -0400

    correct test

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,22 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  before(:each) do
-    @author = Author.create(:name => "Chimamanda Ngozi Adichie")
-  end
+  let(:author) {Author.create(:name => "Chimamanda Ngozi Adichie")}
 
   it "has a name" do
-    expect(@author.name).to eq("Chimamanda Ngozi Adichie")
+    expect(author.name).to eq("Chimamanda Ngozi Adichie")
   end
 
   describe "associations" do
     it "responds to author_books method" do
-      expect(@author).to respond_to(:author_books)
+      expect(author).to respond_to(:author_books)
     end
 
     it "responds to books method" do
-      @author.books.build
-      expect(@author).to have(:no).errors_on(:books)
+      expect(author).to respond_to(:books)
     end
   end
 end
