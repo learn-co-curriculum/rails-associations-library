9eeef56fad72b94335f3eea0e159ae45197b61d1
msuzoagu <msuzoagu@gmail.com>
2015-10-05 15:37:15 -0400

    correct test

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-    let(:author) {Author.create(:name => "Chimamanda Ngozi Adichie")}
-    let(:book) {Book.create(:title => "Half of a Yellow Sun", :description => "With effortless grace, celebrated author Chimamanda Ngozi Adichie illuminates a seminal moment in modern African history: Biafra's impassioned struggle to establish an independent republic in southeastern Nigeria during the late 1960s.")}
+  let(:author) {Author.create(:name => "Chimamanda Ngozi Adichie")}
 
   it "has a name" do
     expect(author.name).to eq("Chimamanda Ngozi Adichie")
