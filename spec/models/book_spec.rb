d81e9e884d78086933f5b898d2a3e63e9127224a
msuzoagu <msuzoagu@gmail.com>
2015-10-05 14:17:02 -0400

    test for a books relationship to author/authors added

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  it 'has a name and a description' do
-    expect {Book.create(:title => "Half of a Yellow Sun", :description => "With effortless grace, celebrated author Chimamanda Ngozi Adichie illuminates a seminal moment in modern African history: Biafra's impassioned struggle to establish an independent republic in southeastern Nigeria during the late 1960s.")}.to_not raise_error
+  before(:all) do
+    @book = Book.create(:title => "Half of a Yellow Sun", :description => "With effortless grace, celebrated author Chimamanda Ngozi Adichie illuminates a seminal moment in modern African history: Biafra's impassioned struggle to establish an independent republic in southeastern Nigeria during the late 1960s.")
+  end
+  it 'has a title' do
+    expect(@book.title).to eq("Half of a Yellow Sun")
+  end
+
+  it "has a description" do
+  expect(@book.description).to eq("With effortless grace, celebrated author Chimamanda Ngozi Adichie illuminates a seminal moment in modern African history: Biafra's impassioned struggle to establish an independent republic in southeastern Nigeria during the late 1960s.")
+  end
+
+  description "associations" do
+    it "responds to author_books method" do
+      expect(@book).to respond_to(:author_books)
+    end
+
+    it "responds to author" do
+      expect(@book).to respond_to(:authors)
+    end
   end
 end
