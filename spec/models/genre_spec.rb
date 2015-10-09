eab60f2515c8ec8ad4be7b53a2efa5b1dd3aa337
msuzoagu <msuzoagu@gmail.com>
2015-10-08 20:06:36 -0400

    test that genre responds to name

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -3,7 +3,15 @@ require 'rails_helper'
 RSpec.describe Genre, type: :model do
   let(genre) {Genre.create(:name => "Fiction")}
 
+  describe "its attributes" do
+    it "has a name" do
+      expect(genre.name).to eq("Fiction")
+    end
+  end
+
   context "relationship to book" do
-    expect(genre).to respond_to(:books)
+    it "responds to books method" do
+      expect(genre).to respond_to(:books)
+    end
   end
 end
