ffafef96231c626174507cfd4c912705f848354d
msuzoagu <msuzoagu@gmail.com>
2015-10-08 20:04:58 -0400

    add relationship between genre and books

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
-  let(genre) {Genre.create(:name => "Fiction")}
+  let(:genre) {Genre.create(:name => "Fiction")}
 
   context "relationship to book" do
-    expect(genre).to respond_to(:books)
+    it "responds to books method" do
+      expect(genre).to respond_to(:books)
+    end
   end
 end
