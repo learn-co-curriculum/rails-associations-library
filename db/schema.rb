08d83c28f4ef0dd714a381fc9cc6845496100e35
msuzoagu <msuzoagu@gmail.com>
2015-10-09 14:39:12 -0400

    establishe relationship between a county and a library

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009125419) do
+ActiveRecord::Schema.define(version: 20151009183226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,10 @@ ActiveRecord::Schema.define(version: 20151009125419) do
     t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "county_id"
   end
+
+  add_index "libraries", ["county_id"], name: "index_libraries_on_county_id", using: :btree
 
   create_table "library_books", force: :cascade do |t|
     t.integer  "library_id"
@@ -83,6 +86,7 @@ ActiveRecord::Schema.define(version: 20151009125419) do
 
   add_foreign_key "author_books", "authors"
   add_foreign_key "author_books", "books"
+  add_foreign_key "libraries", "counties"
   add_foreign_key "library_books", "books"
   add_foreign_key "library_books", "libraries"
 end
