29ecf88f306bb6fc3d7c72eb7e0aa171fab0fdbf
msuzoagu <msuzoagu@gmail.com>
2015-10-08 20:00:21 -0400

    add test for relationship between genre and book

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
-  it 'has a name and a description' do
-    expect {Genre.create(:name => "Fiction")}.to_not raise_error
+  let(genre) {Genre.create(:name => "Fiction")}
+
+  context "relationship to book" do
+    expect(genre).to respond_to(:books)
   end
 end
