cc2191a1276b25811bf8436231b24e3789a05cb3
msuzoagu <msuzoagu@gmail.com>
2015-10-19 17:57:16 -0400

    remove previous migrations

--- db/migrate/04_create_members.rb
+++
@@ -1,9 +0,0 @@
-class CreateMembers < ActiveRecord::Migration
-  def change
-    create_table :members do |t|
-      t.integer :county_id
-      t.string :name
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/08_create_counties.rb
+++
@@ -1,8 +0,0 @@
-class CreateCounties < ActiveRecord::Migration
-  def change
-    create_table :counties do |t|
-      t.string :name
-      t.timestamps null: false
-    end
-  end
-end
