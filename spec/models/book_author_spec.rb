0c3ad8c558648fbf2aaa0742b778954472642101
msuzoagu <msuzoagu@gmail.com>
2015-10-20 14:42:56 -0400

    make model spec singular

--- spec/models/book_author_spec.rb
+++ spec/models/book_author_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe BookAuthors, type: :model do
+RSpec.describe BookAuthor, type: :model do
   let(:author) {Author.create(:name => "Chimamanda Ngozi Adichie")}
 
   let(:book) {Book.create(:title => "Half of a Yellow Sun", :description => "With effortless grace, celebrated author Chimamanda Ngozi Adichie illuminates a seminal moment in modern African history: Biafra's impassioned struggle to establish an independent republic in southeastern Nigeria during the late 1960s.")}
 
-  let(:book_autors) { BookAuthors.create(:book_id => book.id, :author_id => author.id)}
+  let(:book_autors) { BookAuthor.create(:book_id => book.id, :author_id => author.id)}
 
   describe "connection to book model" do
     it "responds to book method" do
